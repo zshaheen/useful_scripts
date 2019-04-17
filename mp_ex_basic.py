213ad0228fad8f214048502ce396abd5e239887c
zshaheen <zshaheen93@gmail.com>
2019-04-17 13:32:21 -0700

    Works with a multiprocessing queue and more.

--- mp_ex_basic.py
+++ mp_ex_basic.py
@@ -9,25 +9,32 @@ class OddEvenMonitor(object):
 
     def __init__(self):
         super(OddEvenMonitor, self).__init__()
-        self.turn = multiprocessing.Value(ctypes.c_char_p, self.ODD_TURN)
+        arr = ['odd' if i%2 == 1 else 'even' for i in range(1, 102)]
+        # self.orders = multiprocessing.Array(ctypes.c_char_p, arr)
+        self.orders = multiprocessing.Queue()
+        for turn in arr:
+            self.orders.put(turn)
+
+        self.manager = multiprocessing.Manager()
+        # self.turn = multiprocessing.Value(ctypes.c_char_p, self._get_next_tar()) 
+        self.turn = self.manager.Value(ctypes.c_char_p, self.orders.get()) 
         self.cv = multiprocessing.Condition()
-    
+
     def wait_turn(self, old_turn):
         with self.cv:
             print('self.turn.value', self.turn.value)
             print('old_turn', old_turn)
             while self.turn.value != old_turn:
                 print('{} is waiting, cause self.turn is {}'.format(old_turn, self.turn.value))
+                print('self.turn.value', self.turn.value)
                 self.cv.wait()
 
     def toggle_turn(self):
         with self.cv:
-            if self.turn.value == self.ODD_TURN:
-                # self.turn = multiprocessing.Value(ctypes.c_char_p, self.EVEN_TURN)
-                self.turn.value = self.EVEN_TURN
-            else:
-                # self.turn = multiprocessing.Value(ctypes.c_char_p, self.ODD_TURN)
-                self.turn.value = self.ODD_TURN
+            # self.turn.value = self._get_next_tar()
+            new_turn = self.orders.get() if not self.orders.empty() else ''
+            print('new_turn', new_turn)
+            self.turn.value = new_turn  # ctypes.c_char_p(new_turn).value
             print('It is now the turn of', self.turn.value)
             self.cv.notify_all()
 
